06c669d23f544b904e5e63a1bb40470223e9c790
ABGEO <takalandzet@gmail.com>
2019-06-26 19:12:42 +0400

    Translate Blog section in Admin-Panel.

--- src/Admin/BlogAdmin.php
+++ src/Admin/BlogAdmin.php
@@ -15,7 +15,6 @@ use Sonata\DoctrineORMAdminBundle\Filter\DateTimeFilter;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\NumberType;
-use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 
 
@@ -43,7 +42,7 @@ final class BlogAdmin extends AbstractAdmin
                 'label' => 'admin.label.tags',
                 'required' => false,
             ])
-            ->add('body', TextareaType::class, [
+            ->add('body', TextType::class, [
                 'label' => 'admin.label.body',
             ])
             ->end()
