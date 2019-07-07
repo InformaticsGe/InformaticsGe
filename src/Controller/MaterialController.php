bd5a3357f92767145ba29fc9847c542ac312274c
ABGEO <takalandzet@gmail.com>
2019-07-07 16:16:17 +0400

    Add single Material Problem page.

--- src/Controller/MaterialController.php
+++ src/Controller/MaterialController.php
@@ -5,16 +5,34 @@ namespace App\Controller;
 use App\Repository\MaterialProblemRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Contracts\Translation\TranslatorInterface;
 
 class MaterialController extends AbstractController
 {
+
+    private $_repository;
+
+    private $_translator;
+
+    /**
+     * MaterialController constructor.
+     *
+     * @param MaterialProblemRepository $repository
+     * @param TranslatorInterface $translator
+     */
+    public function __construct(MaterialProblemRepository $repository, TranslatorInterface $translator)
+    {
+        $this->_repository = $repository;
+        $this->_translator = $translator;
+    }
 
     public function index()
     {
         return $this->render('material/index.html.twig');
     }
 
-    public function problemsList(MaterialProblemRepository $repository, Request $request)
+    public function problemsList(Request $request)
     {
         $parameters = [];
 
@@ -24,7 +42,21 @@ class MaterialController extends AbstractController
         }
 
         return $this->render('material/problems.html.twig', [
-            'problems' => $repository->getList($parameters)
+            'problems' => $this->_repository->getList($parameters)
+        ]);
+    }
+
+    public function singleProblem(int $id)
+    {
+        $problem = $this->_repository
+            ->find($id);
+
+        if (null === $problem) {
+            throw new NotFoundHttpException($this->_translator->trans('problem_not_found', ['%problem_id%' => $id]));
+        }
+
+        return $this->render('material/problem.html.twig', [
+            'problem' => $problem
         ]);
     }
 
