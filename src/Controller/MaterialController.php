4d8bd5166d6df8f519e5d8334e8161ec32963b44
ABGEO <takalandzet@gmail.com>
2019-07-07 14:12:30 +0400

    Add Material Problems listing page.

--- src/Controller/MaterialController.php
+++ src/Controller/MaterialController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\MaterialProblemRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class MaterialController extends AbstractController
@@ -10,6 +11,15 @@ class MaterialController extends AbstractController
     public function index()
     {
         return $this->render('material/index.html.twig');
+    }
+
+    public function problemsList(MaterialProblemRepository $repository)
+    {
+        $problems = $repository->findAll();
+
+        return $this->render('material/problems.html.twig', [
+            'problems' => $problems
+        ]);
     }
 
 }
