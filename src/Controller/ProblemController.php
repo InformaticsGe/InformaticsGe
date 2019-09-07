82eac607a25d80447cd4eeff250accf957f6bc69
ABGEO <takalandzet@gmail.com>
2019-09-07 11:54:27 +0400

    Create Single Problem page.

--- src/Controller/ProblemController.php
+++ src/Controller/ProblemController.php
@@ -4,15 +4,66 @@ namespace App\Controller;
 
 use App\Repository\ProblemRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Contracts\Translation\TranslatorInterface;
 
 class ProblemController extends AbstractController
 {
-    public function list(ProblemRepository $repository)
+    private $_repository;
+    private $_translator;
+
+    /**
+     * ProblemController constructor.
+     *
+     * @param ProblemRepository $repository
+     * @param TranslatorInterface $translator
+     */
+    public function __construct(ProblemRepository $repository, TranslatorInterface $translator)
     {
-        $problems = $repository->findVisibleForListing();
+        $this->_repository = $repository;
+        $this->_translator = $translator;
+    }
+
+    /**
+     * Show Problems listing page.
+     *
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function list()
+    {
+        $problems = $this->_repository
+            ->findVisibleForListing();
 
         return $this->render('problem/list.html.twig', [
             'problems' => $problems,
+        ]);
+    }
+
+    /**
+     * Show single problem.
+     *
+     * @param $id
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function single($id)
+    {
+        $problem = $this->_repository
+            ->find($id);
+
+        // Return 404 page if problem not found or
+        // problem is not visible and user is not a admin.
+        if (
+            null === $problem
+            || (
+                !$problem->getVisible()
+                && !$this->isGranted(['ROLE_SUPER_ADMIN', 'ROLE_ADMIN'])
+            )
+        ) {
+            throw new NotFoundHttpException($this->_translator->trans('problem_not_found', ['%problem_id%' => $id]));
+        }
+
+        return $this->render('problem/problem.html.twig', [
+            'problem' => $problem,
         ]);
     }
 }
